f12fc01fe6795591b37f4e50d217971d6ee2829a
PogudoTanya <34543226+PogudoTanya@users.noreply.github.com>
2019-03-24 20:10:31 +0300

    some cnanges

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  //module.exports = function check(str, bracketsConfig) {
+  var leftbrackets = [];
+  var rightbrackets = [];
+  var bracketsStack = [];
+  var str_brackets = str.split('');
+ 
+  for (var i = 0; i < bracketsConfig.length; i++) {
+    leftbrackets.push(bracketsConfig[i][0]);
+    rightbrackets.push(bracketsConfig[i][1]);
+  }
+
+  
+
+  for (var i = 0; i < str_brackets.length; i++) {
+    var index1 = leftbrackets.indexOf(str_brackets[i]);
+    if (index1 !== -1) {
+      bracketsStack.push(index1);
+    }
+    var index2 = rightbrackets.indexOf(str_brackets[i]);
+    if (index2 !== -1) {
+      index1 = bracketsStack.pop();
+      if (index2 !== index1) {
+        return false;
+      }
+    }
+  }
+
+
+
+ return false;
+}
 }
