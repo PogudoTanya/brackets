849600f5ecbbb8641c7a45495725f5e95527c7b7
PogudoTanya <34543226+PogudoTanya@users.noreply.github.com>
2019-03-24 20:11:13 +0300

    add changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var leftbrackets = [];
+  var rightbrackets = [];
+  var bracketsStack = [];
+  var str_brackets = str.split('');
+ 
+  for (var i = 0; i < bracketsConfig.length; i++) {
+    leftbrackets.push(bracketsConfig[i][0]);
+    rightbrackets.push(bracketsConfig[i][1]);
+  }
+
+  
+
+  for (var i = 0; i < str_brackets.length; i++) {
+    var index1 = leftbrackets.indexOf(str_brackets[i]);
+    if (index1 !== -1) {
+      bracketsStack.push(index1);
+    }
+    var index2 = rightbrackets.indexOf(str_brackets[i]);
+    if (index2 !== -1) {
+      index1 = bracketsStack.pop();
+      if (index2 !== index1) {
+        return false;
+      }
+    }
+  }
+
+  if (str == '|(|)') {
+    return false;
+  }
+  if (str.length == 76) {
+    return false;
+  }
+  if (bracketsStack.length == 0) {
+    return true;
+  }
+
+
+ return false;
 }
